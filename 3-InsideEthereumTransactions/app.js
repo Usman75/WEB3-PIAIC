8d6926ee22706a2a5069793ba18854c56459d46a
Usman Tahir <17611556-004@uogsialkot.edu.pk>
2021-09-28 23:31:05 +0500

    bug fixed ethereumjs-tx

--- 3-InsideEthereumTransactions/app.js
+++ 3-InsideEthereumTransactions/app.js
@@ -1,6 +1,10 @@
-const Tx = require('ethereumjs-tx').Transaction;
+const {  Transaction } = require( '@ethereumjs/tx' );
+const {Chain} = require( '@ethereumjs/common' );
+const Common = require( '@ethereumjs/common' ).default;
 const Web3 = require('web3');
 require('dotenv').config({ path: '../.env' });
+
+async function main () {
 
 const INFURA_KEY = process.env.INFURA_API_KEY;
 const rpcURL = `https://ropsten.infura.io/v3/${INFURA_KEY}`;
@@ -13,27 +17,27 @@ const privatekey1 = Buffer.from(process.env.PRIVATE_KEY_1,'hex');
 const privatekey2 = Buffer.from(process.env.PRIVATE_KEY_2,'hex');
 
 
-web3.eth.getTransactionCount(account1, (err, txCount) => {
-    console.log(txCount);
+web3.eth.getTransactionCount(account2, async (err, txCount) => {
     const txObject = {
-        nonce: web3.utils.toHex(txCount),
-        to: account2,
-        value: web3.utils.toHex(web3.utils.toWei('0.2', 'ether')),
-        gasLimit: web3.utils.toHex(21000),
-        gasPrice: web3.utils.toHex(web3.utils.toWei('20', 'gwei')) 
+        nonce                 :   web3.utils.toHex( txCount ),
+        gasPrice              :   web3.utils.toHex( await web3.eth.getGasPrice() ),
+        gasLimit              :   web3.utils.toHex( await web3.eth.estimateGas({to: account1, data : '0x'})),
+        to                    :   web3.utils.toHex( account1 ),
+        value                 :   web3.utils.toHex( web3.utils.toWei( '0.2' , 'ether' ) ),
+        
     }
-
-    const tx = new Tx(txObject)
-tx.sign(privatekey1);
-
-const serializedTx = tx.serialize();
+    const common = new Common({chain : Chain.Ropsten});
+    const tx =  Transaction.fromTxData(txObject,{ common });
+    const sigenedTx = tx.sign(Buffer.from(privatekey2,'hex'));
+const serializedTx = sigenedTx.serialize();
 const raw = '0x' + serializedTx.toString('hex');
+ const res =  await web3.eth.sendSignedTransaction( raw )
+ .on( 'error' , function( error ) {
+    console.error( error )
+} );
+// console.log(res);
+console.log(`https://ropsten.etherscan.io/tx/${res.transactionHash}`)
+});
+}
 
-web3.eth.sendSignedTransaction(raw, (err, txHash) => {
-    try{
-    console.log('txHash : ',txHash);
-    }catch (err){
-        console.log('Error: ', err);
-    }
-});
-});
+main();
